84c5fbbd4f9284055dd788898879663cc50702e0
Tant4l <szczypiorski.lukasz@gmail.com>
2026-04-02 15:35:06 +0200

    [Fix] Logika kalkulacji podatków w bazie danych

--- db/Stored_Procedures/sp_GetDashboardAggregates.sql
+++ db/Stored_Procedures/sp_GetDashboardAggregates.sql
@@ -6,14 +6,29 @@ CREATE OR ALTER PROCEDURE sp_GetDashboardAggregates
 AS
 BEGIN
     SET NOCOUNT ON;
+
     SELECT
         SUM(RevenueNet) AS GrandTotalRevenueNet,
         SUM(TotalCogsNet) AS TotalCOGS,
         SUM(TotalPackagingNet) AS TotalPackaging,
         SUM(CommissionsNet) AS TotalAllegroCommissions,
         SUM(CourierCostsNet) AS TotalCourierCosts,
-        CAST(SUM(IncomeBeforeTax) * (@IncomeTaxRate / 100.0) AS DECIMAL(12,2)) AS EstimatedIncomeTax,
-        CAST(SUM(IncomeBeforeTax) * (1 - (@IncomeTaxRate / 100.0)) AS DECIMAL(12,2)) AS PureProfitAfterTax
+
+        CAST(
+            CASE
+                WHEN @IncomeTaxRate = 8.50 THEN SUM(RevenueNet) * (@IncomeTaxRate / 100.0)
+                ELSE CASE WHEN SUM(IncomeBeforeTax) > 0 THEN SUM(IncomeBeforeTax) * (@IncomeTaxRate / 100.0) ELSE 0 END
+            END
+        AS DECIMAL(12,2)) AS EstimatedIncomeTax,
+
+        CAST(
+            SUM(IncomeBeforeTax) -
+            CASE
+                WHEN @IncomeTaxRate = 8.50 THEN SUM(RevenueNet) * (@IncomeTaxRate / 100.0)
+                ELSE CASE WHEN SUM(IncomeBeforeTax) > 0 THEN SUM(IncomeBeforeTax) * (@IncomeTaxRate / 100.0) ELSE 0 END
+            END
+        AS DECIMAL(12,2)) AS PureProfitAfterTax
+
     FROM vw_OrderProfitability_Detailed
     WHERE ClientId = @ClientId AND OrderDatePL >= @StartDatePL AND OrderDatePL <= @EndDatePL;
 END;
